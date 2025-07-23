d23df2c756cf51f9a5c0bbfbd297a64a2a928cb5
Prateek <prachteek96@gmail.com>
2025-07-23 18:06:59 +0545

    url fixed

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     // POST to FastAPI
-    const res = await fetch(`https://ajax-demo-wheat.vercel.app${endpoint}`, {
+    const res = await fetch(`https://ajax-demo-backend-ashen.vercel.app${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
